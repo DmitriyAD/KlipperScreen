f919ed474d32cfb539e2a5a03bf3ba2b4a374413
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-28 14:06:20 +0300

    d

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -186,9 +186,7 @@ class BasePanel(ScreenPanel):
         if self._screen.printer.has_heated_bed():
             self.control['temp_box'].pack_start(self.labels['heater_bed_box'], True, True, 3)
             n += 1
-        if self._screen.printer.get_temp_store_devices():
-            self.control['temp_box'].pack_start(self.labels['heat-up'], True, True, 3)
-            n += 1
+    
             
 
         # Options in the config have priority
@@ -210,7 +208,7 @@ class BasePanel(ScreenPanel):
                         name = device
                     for item in titlebar_items:
                         if name == item:
-                            self.control['temp_box'].pack_start(self.labels["%s_box" % device], True, True, 3)
+                            self.control['temp_box'].pack_start(self.labels["%s_box" % 'heat-up'], True, True, 3)
                             n += 1
                             break
 
