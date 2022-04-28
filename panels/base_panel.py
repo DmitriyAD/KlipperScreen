b7c0a7e24ce8198626f57cb8b690ec6cacc633e1
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-28 14:17:39 +0300

    b

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -187,7 +187,7 @@ class BasePanel(ScreenPanel):
             self.control['temp_box'].pack_start(self.labels['heater_bed_box'], True, True, 3)
             n += 1
         if self._screen.printer.get_temp_store_devices():
-            self.control['temp_box'].pack_start(self.labels["temperature_fan"], True, True, 3)
+            self.control['temp_box'].pack_start(self.labels['temperature_fan'], True, True, 3)
             n += 1
             
 
