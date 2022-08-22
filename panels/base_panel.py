db5a963eb943816c62e751ef9e71a56aef18344a
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-22 16:30:02 +0300

    123f

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -128,9 +128,9 @@ class BasePanel(ScreenPanel):
         self.control['vac_set'] = Gtk.Box()
         self.control['vac_set'].set_halign(Gtk.Align.END)
         
-        if self._config.get_menu_items("vac_off") :
+        if self._config.get_menu_name("vac_off") :
             self.control['vacuum'] = Gtk.Label("sosiska")
-        elif self._config.get_menu_items("vac_on") :   
+        elif self._config.get_menu_name("vac_on") :   
             self.control['vacuum'] = Gtk.Label("sosiska2")
         else :
             self.control['vacuum'] = Gtk.Label("sosiska3")    
