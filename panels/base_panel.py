589a1f3c56c1a22d25cf88ef2244ddc2d7a7c171
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-22 17:13:19 +0300

    back

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -128,7 +128,7 @@ class BasePanel(ScreenPanel):
         self.control['vac_set'] = Gtk.Box()
         self.control['vac_set'].set_halign(Gtk.Align.END)
        
-        if  self._ws.klippy.gcode_script("vac_off") :
+        if self._config.get_menu_name("vac_off") :
             self.control['vacuum'] = Gtk.Label("sosiska")   
         elif self._config.get_menu_name("vac_on") :   
             self.control['vacuum'] = Gtk.Label("sosiska2")
