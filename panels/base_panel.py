0e13c1b84da1d9ba9fd740d138f2f4aa05918801
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-23 15:47:57 +0300

    123

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -20,7 +20,7 @@ class BasePanel(ScreenPanel):
         self.title_spacing = self._gtk.font_size * 2
         self.time_update = None
         self.titlebar_name_type = None
-        self.icon = "sos"
+        self.icon = "s"
         self.buttons_showing = {
             'back': False if back else True,
             'macros_shortcut': False,
@@ -153,6 +153,10 @@ class BasePanel(ScreenPanel):
         else:
             self.layout.put(self.titlebar, action_bar_width, 0)
             self.layout.put(self.content, action_bar_width + self.hmargin, self.title_spacing)
+
+    def initialize(self, panel_name):
+        self.update_time()
+        return
     def vacuum_img(self):
         if VacuumPanel.chek_n == True:
             self.icon = "sosiskaON"
@@ -160,10 +164,10 @@ class BasePanel(ScreenPanel):
             self.icon = "sosiskaOFF"
         else:
             self.icon = "sosiskaNothing"
-            
-    def initialize(self, panel_name):
-        self.update_time()
-        return
+
+
+
+
     def show_heaters(self, show=True):
         for child in self.control['temp_box'].get_children():
             self.control['temp_box'].remove(child)
