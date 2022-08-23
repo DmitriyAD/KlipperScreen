cec186585a4bc3d58f2dc790d68b646e58b45f62
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-23 11:19:23 +0300

    dasd

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -78,9 +78,9 @@ class BasePanel(ScreenPanel):
             else:
                 self.control_grid.attach(self.control['space%s' % i], 0, i, 1, 1)
         if self._screen.vertical_mode:
-            self.control_grid.attach(self.control['estop'], 5, 0, 1, 1)
+            self.control_grid.attach(self.control['estop'], 7, 0, 1, 1)
         else:
-            self.control_grid.attach(self.control['estop'], 0, 5, 1, 1)
+            self.control_grid.attach(self.control['estop'], 0, 7, 1, 1)
         if self._screen.vertical_mode:
             self.control_grid.attach(self.control['shutdown'], 4, 0, 1, 1)
         else:
