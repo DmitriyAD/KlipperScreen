843d64afa5a60e12923185771db7c8ecd3ce3712
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-23 11:53:26 +0300

    123

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -127,8 +127,8 @@ class BasePanel(ScreenPanel):
 
         self.control['vac_set'] = Gtk.Box()
         self.control['vac_set'].set_halign(Gtk.Align.END)
-        vacumsersh = self._printer.get_config_section_list("vac_on")
-        if   vacumsersh == [] :
+       
+        if self._config.get_menu_name("vac_off") :
             self.control['vacuum'] = Gtk.Label("sosiska")
    
         elif self._config.get_menu_name("vac_on") :   
