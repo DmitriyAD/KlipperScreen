cb2698a94e89f88a5e5321797b733c9abfdc8f6a
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-23 13:28:36 +0300

    123

--- screen.py
+++ screen.py
@@ -921,7 +921,7 @@ class KlipperScreen(Gtk.Window):
             os.system("sudo shutdown -P now")
         widget.destroy()
     def vac(self):
-         self._ws.klippy.gcode_script("vac_on")
+        self._ws.klippy.gcode_script("vac_on")
               
 
            
