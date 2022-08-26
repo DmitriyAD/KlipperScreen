24843af0bbaa3a8237264f50968a34dc91b22696
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-26 14:51:13 +0300

    1234

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -130,7 +130,7 @@ class BasePanel(ScreenPanel):
 
         self.control['vac_set'] = Gtk.Box()
         self.control['vac_set'].set_halign(Gtk.Align.END)
-        self.control['vacuum'] = self._gtk.ButtonImage('fan', None, None , 1)  
+        self.control['vacuum'] = self._gtk.Image('fan', None, None , 1)  
       
         self.control['vac_set'].pack_end(self.control['vacuum'], True, True, self.hmargin)
 
@@ -416,9 +416,9 @@ class BasePanel(ScreenPanel):
             
     def update_imgVacuum(self):
         if VacuumPanel.vac_on == True:
-            self.control['vacuum'].set._gtk.ButtonImage('vac_on', None, None , 1) 
+            self.control['vacuum'].set._gtk.Image('vac_on', None, None , 1) 
         elif VacuumPanel.vac_off == False:
-            self.control['vacuum'].set.ButtonImage('vac_off', None, None , 1)  
+            self.control['vacuum'].set.Image('vac_off', None, None , 1)  
 
         # if VacuumPanel.vac_on == True:
         #     self.control['vacuum'] = self._gtk.ButtonImage('vac_on', None, None , 1) 
