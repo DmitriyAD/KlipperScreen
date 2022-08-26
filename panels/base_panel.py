1b1edb0fcf04fbae4746c61385941a7b6e132dbf
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-08-26 14:49:15 +0300

    1123

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -419,8 +419,6 @@ class BasePanel(ScreenPanel):
             self.control['vacuum'].set._gtk.ButtonImage('vac_on', None, None , 1) 
         elif VacuumPanel.vac_off == False:
             self.control['vacuum'].set.ButtonImage('vac_off', None, None , 1)  
-        else:
-            self.control['vacuum'].set('home', None, None , 1) 
 
         # if VacuumPanel.vac_on == True:
         #     self.control['vacuum'] = self._gtk.ButtonImage('vac_on', None, None , 1) 
