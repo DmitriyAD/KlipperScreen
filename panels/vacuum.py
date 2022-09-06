d1a91a9728513e0719375c410ffa92d90fa8d205
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-09-06 12:47:39 +0300

    1234

--- panels/vacuum.py
+++ panels/vacuum.py
@@ -33,7 +33,6 @@ class VacuumPanel(ScreenPanel):
     def vac_on(self, widget):
         self.vaccheck = 1
         self._screen._ws.klippy.gcode_script("vac_on")
-        BasePanel.icon = 2
         return True
         
     def vac_off(self, widget):
