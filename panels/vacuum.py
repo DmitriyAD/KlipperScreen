c6a10366facad08b649ec8fc83df6ecb67b23aa6
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-09-15 10:40:03 +0300

    bar

--- panels/vacuum.py
+++ panels/vacuum.py
@@ -33,8 +33,9 @@ class VacuumPanel(ScreenPanel):
         self._screen._ws.klippy.gcode_script("vac_on")
         self._base.update_imgVacuum(1)
 
+
     def vac_off(self, widget):
         self._screen._ws.klippy.gcode_script("vac_off") 
         self._base.update_imgVacuum(2)
-           
+        self._base.update_imgVacuumOFF
         
