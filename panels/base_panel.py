b2d275e39b989250698634c78811567ee251104e
DmitriyAD <95650178+DmitriyAD@users.noreply.github.com>
2022-09-28 13:06:12 +0300

    macros disable

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -21,7 +21,7 @@ class BasePanel(ScreenPanel):
         self.titlebar_name_type = None
         self.buttons_showing = {
             'back': not back,
-            'macros_shortcut': False,
+            'macros_shortcut': True,
             'printer_select': False
         }
         self.current_extruder = None
@@ -45,9 +45,9 @@ class BasePanel(ScreenPanel):
         self.control['estop'].connect("clicked", self.emergency_stop)
         _ = self.lang.gettext
         script = {"script":"M81"}
-        self.control['shutdown'] = self._gtk.ButtonImage('shutdown', None, None, 1)
+        self.control['shutdown'] = self._gtk.ButtonImage('shutdown', scale = 1)
         self.control['shutdown'].connect ( "clicked", self._screen._confirm_send_action, _("Are you sure you wish to shutdown the system?"), "printer.gcode.script", script)
-        self.control['wifi'] = self._gtk.ButtonImage('network', None, None, 1)
+        self.control['wifi'] = self._gtk.ButtonImage('network', scale = 1)
         self.control['wifi'].connect("clicked", self.menu_item_clicked, "network",{
                 "name": _('Network'),
                 "panel": "network"
